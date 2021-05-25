9dcb04d8e1424516f8cb443f55e93e0bdcc68e94
Jannis R <mail@jannisr.de>
2021-05-25 15:49:58 +0100

    gen-location-groups-txt.js

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,4 +5,5 @@ set -x
 
 generate-booking-rules-txt /app/herrenberg-flex-rules.js *.txt | tee booking_rules.txt | wc -l
 generate-locations-geojson /app/herrenberg-flex-rules.js *.txt | tee locations.geojson | wc -l
+generate-location-groups-txt /app/herrenberg-flex-rules.js *.txt | tee location_groups.txt | wc -l
 patch-stop-times-txt /app/herrenberg-flex-rules.js *.txt | sponge stop_times.txt
